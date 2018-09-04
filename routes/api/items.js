5ad7eeed09fa027887bb1dacb12bea0b6ea6b63b
Lennard Austrup <lennardaustrup@gmx.de>
2018-09-05 01:56:25 +0200

    store.js on server fix

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Item = require('../../models/Items');
+const Item = require('../../models/items');
 
 // GET
 router.get('/', (req, res) => {
