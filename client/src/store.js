9681d8efb6151b91b60f00a6519ae4010109a533
Lennard Austrup <lennardaustrup@gmx.de>
2018-09-05 01:46:31 +0200

    store.js on server fix

--- client/src/store.js
+++ client/src/store.js
@@ -1,19 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducer';
+import combineReducer from './reducers/rootReducer';
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers =  typeof window === 'object' &&  window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
-
-const composingMiddlewareAndDevTools = composeEnhancers(applyMiddleware(...middleware));
-
-const store = createStore(rootReducer, initialState, composingMiddlewareAndDevTools);
-
-console.log(store.getState());
-
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const store = createStore(combineReducer, initialState, composeEnhancers(
+    applyMiddleware(...middleware)
+));
 
 export default store;
